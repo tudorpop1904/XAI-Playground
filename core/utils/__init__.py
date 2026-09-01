bd675fc59267cc70cf6449be4a1bbe9cd22156c4
tudorpop1904 <Pop.Io.Stefan@student.utcluj.ro>
2026-09-02 01:00:47 +0300

    Refactoring paths

--- core/utils/__init__.py
+++ core/utils/__init__.py
@@ -2,7 +2,7 @@
 
 from .custom_dataset import FileListDataset
 from .logger import get_logger
-from .paths import BASE_DIR, MODELS_DIR, DB_DIR
+from .paths import BASE_DIR, MODELS_DIR, DB_DIR, DATASETS_DIR, IMAGES_DIR
 from .metrics import track_hardware
 from .image_features import compute_fft, compute_lbp, compute_sobel, add_forensic_channels
 
@@ -12,6 +12,8 @@ __all__ = [
     "BASE_DIR",
     "MODELS_DIR",
     "DB_DIR",
+    "DATASETS_DIR",
+    "IMAGES_DIR",
     "track_hardware",
     "compute_fft",
     "compute_lbp",
--- core/utils/paths.py
+++ core/utils/paths.py
@@ -3,3 +3,5 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 MODELS_DIR = BASE_DIR / "storage" / "models"
 DB_DIR = BASE_DIR / "storage"
+DATASETS_DIR = BASE_DIR / "storage" / "datasets"
+IMAGES_DIR = BASE_DIR / "storage" / "images"
